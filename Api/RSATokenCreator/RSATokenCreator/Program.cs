a749397acba6013888e6a231ce3b430d1bbf3c2a
Rasmus Netland <forfb@hotmail.no>
2025-08-20 08:49:33 +0200

    Added RSATokenCreator, tool used for RSA token creation

---
+++ Api/RSATokenCreator/RSATokenCreator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+
+namespace RSATokenCreator
+{
+    // TOOL FOR GENERATING RSA KEY USED IN API
+    class Program
+    {
+        static void Main()
+        {
+            using (RSA rsa = RSA.Create(4096)) // 4096-bit key is strong
+            {
+                // Private + public key
+                string privateKeyXml = rsa.ToXmlString(true);
+
+                // Public key only (for verification)
+                string publicKeyXml = rsa.ToXmlString(false);
+
+                Console.WriteLine("Private key (FullTokenKey):");
+                Console.WriteLine(privateKeyXml);
+                Console.WriteLine("\nPublic key (for verification):");
+                Console.WriteLine(publicKeyXml);
+            }
+        }
+    }
+}
